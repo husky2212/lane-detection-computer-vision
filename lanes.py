04b56a43964e8534cfafc7892ccc3ecbdc59fc8f
Keshav <52997366+husky2212@users.noreply.github.com>
2020-04-04 07:38:54 +0530

    Add files via upload

---
+++ lanes.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def canny(image):
+	  gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+	  blur  = cv2.GaussianBlur(gray, (5, 5) , 0)
+	  canny  = cv2.Canny(blur, 50, 150);
+	  return canny
+
+
+
+def display_lines(image, lines):
+	line_image =np.zeros_like(image)
+	if lines is not None:
+		for line in lines:
+			x1, y1, x2, y2 = line.reshape(4)
+			cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+	return line_image
+
+def region_of_interest(image):
+	height = image.shape[0]
+	polygons = np.array([
+		[(200, height), (420, height), (270,350)]
+		])
+	mask=np.zeros_like(image)
+	cv2.fillPoly(mask, polygons, 255)
+	masked_image=cv2.bitwise_and(image,mask)   
+	return masked_image
+
+image = cv2.imread('gunalan.jpg')
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+lines=cv2.HoughLinesP(cropped_image,2, np.pi/180, 100, np.array([]),minLineLength=10,maxLineGap=100)
+line_image = display_lines(lane_image, lines)
+cv2.imshow("result",line_image)
+cv2.waitKey(0)  
